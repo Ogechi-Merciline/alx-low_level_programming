a582b46ed9ee9c5ae083d95d4aa950c8b1b152a3
Ogechi <ogechimercyline2@gmail.com>
2023-03-14 14:46:28 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,6 @@
 int **alloc_grid(int width, int height)
 {
 	int **mee;
-
 	int x, y;
 
 	if (width <= 0 || height <= 0)
@@ -21,31 +20,33 @@ int **alloc_grid(int width, int height)
 	mee = malloc(sizeof(int *) * height);
 
 	if (mee == NULL)
+
 		return (NULL);
 
 	for (x = 0; x < height; x++)
-
 	{
 		mee[x] = malloc(sizeof(int) * width);
 
 		if (mee[x] == NULL)
-
 		{
 			for (; x >= 0; x--)
 
 				free(mee[x]);
+
 			free(mee);
 
 			return (NULL);
+
 		}
 
-	for (x = 0; x < height; x++)
+	}
 
+	for (x = 0; x < height; x++)
 	{
 		for (y = 0; y < width; y++)
 
 			mee[x][y] = 0;
 	}
+
 	return (mee);
-	}
 }
