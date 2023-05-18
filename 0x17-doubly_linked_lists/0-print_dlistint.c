2ccb88ad77a3d42b4647a87db85a7824b958d728
root <ogechimercyline@gmail.com>
2023-05-18 08:36:52 -0700

    prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ * @h - pointer to the list_t list to print
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h);
+{
+	const dlistint_t *h = h;
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (h);
+}
