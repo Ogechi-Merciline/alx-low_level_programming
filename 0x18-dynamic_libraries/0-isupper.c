3e4500fe745b61e01d19808cabcff1edf55480e2
root <ogechimercyline@gmail.com>
2023-05-22 10:11:49 -0700

    update

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - uppercase letters
  * @c: char to check
+ *
  * Return: 0 or 1
  */
 
-int_isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -6,10 +6,8 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
-
 int main(void)
 {
 	printf("%d\n", _strlen("My Dyn Lib"));
-
 	return (EXIT_SUCCESS);
 }
